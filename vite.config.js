41077cb6420b8387997ff9e707dfc6d888cf24fc
Geervan <geervan99@gmail.com>
2026-02-05 17:09:09 +0530

    Landing page done

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
+                main: resolve(__dirname, 'landing.html'),
+                home:resolve(__dirname, 'index.html'),
                 lore: resolve(__dirname, 'lore.html'),
                 chat: resolve(__dirname, 'chat.html'),
             },
